bc5df350997a59bd9b1ca687db354ce8fab209e4
Rifky Syahreza Fahlevi S <37953823+rifkysyahreza@users.noreply.github.com>
2024-11-04 11:09:10 +0700

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -2,15 +2,6 @@
 
 CREATE SCHEMA montrack AUTHORIZATION myuser;
 
--- DROP SEQUENCE montrack.active_wallet_id_seq;
-
-CREATE SEQUENCE montrack.active_wallet_id_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
 -- DROP SEQUENCE montrack.goals_history_id_seq;
 
 CREATE SEQUENCE montrack.goals_history_id_seq
@@ -73,20 +64,7 @@ CREATE SEQUENCE montrack.wallet_id_seq
 	MAXVALUE 2147483647
 	START 1
 	CACHE 1
-	NO CYCLE;-- montrack.active_wallet definition
-
--- Drop table
-
--- DROP TABLE montrack.active_wallet;
-
-CREATE TABLE montrack.active_wallet (
-	id serial4 NOT NULL,
-	wallet_id int4 NOT NULL,
-	CONSTRAINT active_wallet_pkey PRIMARY KEY (id)
-);
-
-
--- montrack.goals definition
+	NO CYCLE;-- montrack.goals definition
 
 -- Drop table
 
@@ -94,7 +72,7 @@ CREATE TABLE montrack.active_wallet (
 
 CREATE TABLE montrack.goals (
 	id serial4 NOT NULL,
-	wallet_id int4 NOT NULL,
+	wallet_id int4 NULL,
 	"name" varchar NULL,
 	target_amount numeric(12, 2) NULL,
 	current_amount numeric(12, 2) NULL,
@@ -113,7 +91,7 @@ CREATE TABLE montrack.goals (
 
 CREATE TABLE montrack.goals_history (
 	id serial4 NOT NULL,
-	goals_id int4 NOT NULL,
+	goals_id int4 NULL,
 	"name" varchar NULL,
 	amount numeric(12, 2) NULL,
 	"date" timestamp NULL,
@@ -129,7 +107,7 @@ CREATE TABLE montrack.goals_history (
 
 CREATE TABLE montrack.money_records (
 	id serial4 NOT NULL,
-	pocket_id int4 NOT NULL,
+	pocket_id int4 NULL,
 	"name" varchar NULL,
 	"type" varchar NULL,
 	"date" timestamp NULL,
@@ -148,7 +126,7 @@ CREATE TABLE montrack.money_records (
 
 CREATE TABLE montrack.notification (
 	id serial4 NOT NULL,
-	user_id int4 NOT NULL,
+	user_id int4 NULL,
 	title varchar NULL,
 	description text NULL,
 	is_read bool NULL,
@@ -165,10 +143,11 @@ CREATE TABLE montrack.notification (
 
 CREATE TABLE montrack.pocket (
 	id serial4 NOT NULL,
-	wallet_id int4 NOT NULL,
+	wallet_id int4 NULL,
 	emoji_unicode varchar NULL,
 	"name" varchar NULL,
-	amount numeric(12, 2) NULL,
+	allocated_amount numeric(12, 2) NULL,
+	used_amount numeric(12, 2) NULL,
 	description text NULL,
 	CONSTRAINT pocket_pkey PRIMARY KEY (id)
 );
@@ -182,17 +161,16 @@ CREATE TABLE montrack.pocket (
 
 CREATE TABLE montrack."user" (
 	id serial4 NOT NULL,
-	active_wallet_id int4 NULL,
+	wallet_id int4 NULL,
 	"name" varchar NULL,
 	profile_photo varchar NULL,
-	email varchar NOT NULL,
-	"password" varchar NOT NULL,
+	email varchar NULL,
+	"password" varchar NULL,
 	pin varchar NULL,
 	"quotes" varchar NULL,
 	oauth_provider varchar NULL,
 	created_at timestamp NULL,
 	updated_at timestamp NULL,
-	CONSTRAINT user_email_key UNIQUE (email),
 	CONSTRAINT user_pkey PRIMARY KEY (id)
 );
 
@@ -205,16 +183,11 @@ CREATE TABLE montrack."user" (
 
 CREATE TABLE montrack.wallet (
 	id serial4 NOT NULL,
-	user_id int4 NOT NULL,
+	user_id int4 NULL,
 	"name" varchar NULL,
 	balance numeric(12, 2) NULL,
 	CONSTRAINT wallet_pkey PRIMARY KEY (id)
 );
-
-
--- montrack.active_wallet foreign keys
-
-ALTER TABLE montrack.active_wallet ADD CONSTRAINT fk_active_wallet_wallet FOREIGN KEY (wallet_id) REFERENCES montrack.wallet(id);
 
 
 -- montrack.goals foreign keys
@@ -244,9 +217,9 @@ ALTER TABLE montrack.pocket ADD CONSTRAINT fk_pocket_wallet FOREIGN KEY (wallet_
 
 -- montrack."user" foreign keys
 
-ALTER TABLE montrack."user" ADD CONSTRAINT fk_user_active_wallet FOREIGN KEY (active_wallet_id) REFERENCES montrack.active_wallet(id);
+ALTER TABLE montrack."user" ADD CONSTRAINT fk_wallet_user FOREIGN KEY (wallet_id) REFERENCES montrack.wallet(id);
 
 
 -- montrack.wallet foreign keys
 
-ALTER TABLE montrack.wallet ADD CONSTRAINT fk_wallet_user FOREIGN KEY (user_id) REFERENCES montrack."user"(id);
+ALTER TABLE montrack.wallet ADD CONSTRAINT fk_user_wallet FOREIGN KEY (user_id) REFERENCES montrack."user"(id);
